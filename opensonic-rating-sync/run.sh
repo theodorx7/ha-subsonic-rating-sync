2e4043af71552b367cb3f90295e123e53086ac85
Fedor <99128059+theodorx7@users.noreply.github.com>
2026-08-14 00:15:24 +0700

    Update run.sh

--- opensonic-rating-sync/run.sh
+++ opensonic-rating-sync/run.sh
@@ -3,9 +3,6 @@ set -e
 
 bashio::log.info "Starting Navidrome/Subsonic Rating Sync App..."
 
-# Экспортируем только PYTHONPATH, он нужен для поиска вашего пакета
 export PYTHONPATH="/app/"
 
-# Запускаем Python
-# Флаг -u отключает буферизацию, чтобы логи сразу летели в HA
 exec python3 -u -m opensonic_rating_sync
